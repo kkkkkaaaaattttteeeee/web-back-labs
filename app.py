57fc01ebfb54eaf3dfddea202a609145cf555c53
Обедина Екатерина <catherine.ob@mail.ru>
2025-10-10 19:50:00 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -330,4 +330,11 @@ def example():
     name = 'Екатерина Обедина'
     nomer = 2
     nomer_cursa = 3
-    return render_template('example.html', name=name, nomer=nomer, nomer_cursa=nomer_cursa)
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html', name=name, nomer=nomer, nomer_cursa=nomer_cursa, fruits=fruits)
