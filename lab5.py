41ef55249f13dc92bbb92fd0ed51a2a76525ec59
Обедина Екатерина <catherine.ob@mail.ru>
2025-11-08 09:36:16 +0700

    Заготовка лабораторной 5

---
+++ lab5.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, request, render_template, redirect, session
+lab5 = Blueprint('lab5', __name__)
+
+
+@lab5.route('/lab5/')
+def lab():
+    return render_template('lab5/lab5.html')
+
