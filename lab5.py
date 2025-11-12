e5b1278b94882a481570b5136dc291330b4cdbc5
Обедина Екатерина <catherine.ob@mail.ru>
2025-11-13 00:06:15 +0700

    Хеширование паролей

--- lab5.py
+++ lab5.py
@@ -2,11 +2,27 @@ from flask import Blueprint, request, render_template, redirect, session
 lab5 = Blueprint('lab5', __name__)
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
 
 @lab5.route('/lab5/')
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
+def db_connect():
+    conn = psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'knowledge_base_df',  
+            user='obedina_ekaterina_knowledge_base',
+            password='123'
+        )
+    cur = conn.cursor(cursor_factory= RealDictCursor)
+
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -16,42 +32,31 @@ def register():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    # ИСПРАВЛЕНА ПРОВЕРКА (было if not (login or password))
     if not login or not password:
         return render_template('lab5/register.html', error='Заполните все поля')
     
     try:
-        # ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ
-        conn = psycopg2.connect(
-            host='127.0.0.1',
-            database='knowledge_base_df',  
-            user='obedina_ekaterina_knowledge_base',
-            password='123'
-        )
-        cur = conn.cursor()
-
-        # ИСПРАВЛЕНА SQL-ИНЪЕКЦИЯ (не используйте f-строки!)
+        # ИСПОЛЬЗУЕМ ФУНКЦИЮ ДЛЯ ПОДКЛЮЧЕНИЯ
+        conn, cur = db_connect()
+        
         cur.execute("SELECT login FROM users WHERE login = %s", (login,))
         if cur.fetchone():
-            cur.close()
-            conn.close()
+            db_close(conn, cur)
             return render_template('lab5/register.html', error="Такой пользователь уже существует")
 
-        # ИСПРАВЛЕНА SQL-ИНЪЕКЦИЯ И ДОБАВЛЕНО ХЕШИРОВАНИЕ
         from werkzeug.security import generate_password_hash
         hashed_password = generate_password_hash(password)
         
         cur.execute("INSERT INTO users (login, password) VALUES (%s, %s)", 
                    (login, hashed_password))
-        conn.commit()
-        cur.close()
-        conn.close()
+        
+        # ИСПОЛЬЗУЕМ ФУНКЦИЮ ДЛЯ ЗАКРЫТИЯ
+        db_close(conn, cur)
         
         return render_template('lab5/success.html', login=login)
         
     except Exception as e:
         return render_template('lab5/register.html', error=f'Ошибка базы данных: {str(e)}')
-
 
 @lab5.route('/lab5/login', methods = ['GET', 'POST'])
 def login():
@@ -65,31 +70,25 @@ def login():
         return render_template('lab5/login.html', error="Заполните поля")
     
     try:
-        conn = psycopg2.connect(
-            host = '127.0.0.1',
-            database = 'knowledge_base_df',  
-            user='obedina_ekaterina_knowledge_base',
-            password='123'
-        )
-        cur = conn.cursor(cursor_factory= RealDictCursor)
+        # ИСПОЛЬЗУЕМ ФУНКЦИЮ ДЛЯ ПОДКЛЮЧЕНИЯ
+        conn, cur = db_connect()
         
         cur.execute("SELECT * FROM users WHERE login= %s", (login,))
         user = cur.fetchone()
 
         if not user:
-            cur.close()
-            conn.close()
+            db_close(conn, cur)
             return render_template('lab5/login.html', error='Логин и/или пароль неверны')
         
         from werkzeug.security import check_password_hash
         if not check_password_hash(user['password'], password):
-            cur.close()
-            conn.close()
+            db_close(conn, cur)
             return render_template('lab5/login.html', error='Логин и/или пароль неверны')
         
         session['user_login'] = login
-        cur.close()
-        conn.close()
+        
+        # ИСПОЛЬЗУЕМ ФУНКЦИЮ ДЛЯ ЗАКРЫТИЯ
+        db_close(conn, cur)
     
         return render_template('lab5/success_login.html', login=login)
 
