8385032c0427b74d958fafcbbe7aa4e4b3c6537c
Обедина Екатерина <catherine.ob@mail.ru>
2025-11-22 09:07:27 +0700

    Заготовка к 6 лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 
 app = Flask(__name__)
@@ -19,6 +20,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 
 @app.route("/")
@@ -40,6 +42,7 @@ def index():
             <li><a href="/lab3">Лабораторная работа 3</a></li>
             <li><a href="/lab4">Лабораторная работа 4</a></li>
             <li><a href="/lab5">Лабораторная работа 5</a></li>
+            <li><a href="/lab6">Лабораторная работа 6</a></li>
         </ul>
     </nav>
     <footer>
